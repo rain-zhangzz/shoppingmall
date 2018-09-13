26f477d88f6074199fc6ebd293ae422c4520efed
张志政 <zhangzhizheng@zhangzhizhengdeMacBook-Air.local>
2018-09-13 10:55:30 +0800

    init the project

---
+++ top.js
@@ -0,0 +1,55 @@
+Vue.component("top",{
+    template:"<nav class=\"navbar navbar-default\" role=\"navigation\">\n" +
+    "        <div class=\"container-fluid\">\n" +
+    "            <div class=\"navbar-header\">\n" +
+    "                <img class=\"logo\" src=\"../image/handmap.png\" alt=\"\">\n" +
+    "            </div>\n" +
+    "            <div>\n" +
+    "                <!--向右对齐-->\n" +
+    "                <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "                    <li class=\"dropdown\">\n" +
+    "                        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                            首页 <b class=\"caret\"></b>\n" +
+    "                        </a>\n" +
+    "                        <ul class=\"dropdown-menu\">\n" +
+    "                            <li><a href=\"#\">数据统计</a></li>\n" +
+    "                        </ul>\n" +
+    "                    </li>\n" +
+    "                    <li class=\"dropdown\">\n" +
+    "                        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                            内容管理 <b class=\"caret\"></b>\n" +
+    "                        </a>\n" +
+    "                        <ul class=\"dropdown-menu\">\n" +
+    "                            <li><a href=\"#\">用户管理</a></li>\n" +
+    "                            <li><a href=\"#\">轨迹管理</a></li>\n" +
+    "                            <li><a href=\"#\">勘路笔记</a></li>\n" +
+    "                            <li><a href=\"#\">打点管理</a></li>\n" +
+    "                            <li><a href=\"#\">商城管理</a></li>\n" +
+    "                            <li><a href=\"#\">图层管理</a></li>\n" +
+    "                        </ul>\n" +
+    "                    </li>\n" +
+    "                    <li class=\"dropdown\">\n" +
+    "                        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                            APP管理 <b class=\"caret\"></b>\n" +
+    "                        </a>\n" +
+    "                        <ul class=\"dropdown-menu\">\n" +
+    "                            <li><a href=\"#\">广告展示</a></li>\n" +
+    "                            <li><a href=\"#\">话题管理</a></li>\n" +
+    "                            <li><a href=\"#\">消息管理</a></li>\n" +
+    "                            <li><a href=\"#\">活动管理</a></li>\n" +
+    "                            <li><a href=\"#\">意见反馈</a></li>\n" +
+    "                        </ul>\n" +
+    "                    </li>\n" +
+    "                    <li class=\"dropdown\">\n" +
+    "                        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                            登出 <b class=\"caret\"></b>\n" +
+    "                        </a>\n" +
+    "                        <ul class=\"dropdown-menu\">\n" +
+    "                            <li><a href=\"#\">登录</a></li>\n" +
+    "                        </ul>\n" +
+    "                    </li>\n" +
+    "                </ul>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </nav>"
+})
